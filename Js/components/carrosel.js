72913d129642ab691a1ae46e6c624e1eb78fd708
Ramonuriel <Ramonuriel13@gmail.com>
2022-10-17 00:29:15 -0300

    create responsive large smartphone

--- Js/components/carrosel.js
+++
@@ -1,7 +0,0 @@
-const btnRight = window.document.querySelector('.button-arrow.-right')
-
-const logoCards = window.document.querySelector('.logo-cards')
-
-btnRight.addEventListener('click', function () {
-  console.log(logoCards)
-})
