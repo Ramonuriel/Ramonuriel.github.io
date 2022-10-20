332ae8ad25b4dd22ad2313683c3de4c2c41a06a1
Ramonuriel <Ramonuriel13@gmail.com>
2022-10-20 17:40:09 -0300

    att menu hamburguer

--- Js/menu.js
+++ Js/menu.js
@@ -1,10 +1,13 @@
 // Menu Responsivo //
-let show = true
-
 const menu = document.querySelector('.menu')
-const menuToggle = menu.querySelector('.menu-toggle')
 
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('on', show)
-  show = !show
-})
+function menuToggle(event) {
+  if (event.type === 'touchstart') event.preventDefalut()
+  {
+  }
+  const menuToggle = document.getElementById('.menu-toggle')
+  menu.classList.toggle('on')
+}
+
+menu.addEventListener('click', menuToggle)
+menu.addEventListener('touchstart', menuToggle)
