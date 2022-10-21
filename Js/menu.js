fe8d8f4fff27e95b99f534d75f311f891235878d
Ramonuriel <Ramonuriel13@gmail.com>
2022-10-20 21:08:29 -0300

    remover bug menu

--- Js/menu.js
+++ Js/menu.js
@@ -5,8 +5,8 @@ const menu = document.querySelector('.menu')
 const menuToggle = menu.querySelector('.menu-toggle')
 
 menuToggle.addEventListener('click', () => {
-  document.body.style.overflow = show ? 'hidden' : 'initial'
-
+  /* document.body.style.overflow = show ? 'hidden' : 'initial'
+   */
   menu.classList.toggle('on', show)
 
   show = !show
